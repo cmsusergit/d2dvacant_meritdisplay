e45aa3baec7d0171d2ebebd3c54733fe435c256c
cmsusergit <45680530+cmsusergit@users.noreply.github.com>
2023-09-14 15:26:36 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello!');
+const fetchmerit = async (contact) => {
+  const url1 = `https://mhazmbcbujixalspvqrz.supabase.co/rest/v1/VacantMeritTbl?contact=eq.${contact}&select=*`;
+  try {
+    console.log(url1);
+    const response = await fetch(url1, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        apikey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oYXptYmNidWppeGFsc3B2cXJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUyNDc4NDksImV4cCI6MTk5MDgyMzg0OX0.IsZaoHFa9ovwI_z0ZhhqucJlCHZ-dZRqzar7d4sdBAw',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oYXptYmNidWppeGFsc3B2cXJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUyNDc4NDksImV4cCI6MTk5MDgyMzg0OX0.IsZaoHFa9ovwI_z0ZhhqucJlCHZ-dZRqzar7d4sdBAw',
+      },
+    });
+    const dt = await response.json();
+    return { error: false, dt: dt };
+  } catch (ee) {
+    return { error: true, dt: ee };
+  }
+};
